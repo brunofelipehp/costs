4622174c8393e26e79d948a7a8896a26c5b391a4
Bruno Felipe <brunofelipehp8@gmail.com>
2022-12-27 15:43:04 -0300

    registered project data editing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from  './components/pages/Home'
-import { Projects } from  './components/pages/Projects'
-import { Company } from  './components/pages/Company'
-import { Contact } from  './components/pages/Contact'
-import { NewProject } from  './components/pages/NewProject'
-import { Container } from  './components/layout/Container'
+import { Home } from './components/pages/Home'
+import { Projects } from './components/pages/Projects'
+import { Company } from './components/pages/Company'
+import { Contact } from './components/pages/Contact'
+import { NewProject } from './components/pages/NewProject'
+import { Project } from './components/pages/Project'
+
+import { Container } from './components/layout/Container'
 import { Navbar } from './components/layout/Navbar'
 import { Footer } from './components/layout/Footer'
 
 function App() {
-  return ( 
+  return (
     <Router>
-     <Navbar />
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/projects' element={<Projects />} />
-          <Route exact path='contact/' element={<Contact />} />
-          <Route exact path='/company' element={<Company />} />
-          <Route exact path='/newproject' element={<NewProject />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="contact/" element={<Contact />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
-    </Router> 
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
