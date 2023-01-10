958dcf83db4e2d6a4d40f4ba44c0974c91577af4
Bruno Felipe <brunofelipehp8@gmail.com>
2023-01-10 14:05:17 -0300

    removing services

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -95,12 +95,39 @@ export function Project() {
       .then((data) => {
         //exibir os serviços
         //console.log(data)
-        showServiceForm(false)
+        setShowServiceForm(false)
       })
       .catch((err) => console.log(err))
   }
 
-  function removeService() {}
+  function removeService(id, cost) {
+    setMessage('')
+
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    )
+
+    const projectUpdated = project
+
+    projectUpdated.services = servicesUpdated
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(projectUpdated)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setProject(projectUpdated)
+      setServices(servicesUpdated)
+      setMessage('Serviço removido com sucesso!')
+      setType('success')
+    })
+    .catch(err => console.log(err))
+    }
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm)
